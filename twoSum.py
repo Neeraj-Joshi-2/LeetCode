11cf89c3bb642b0710113f5f135814e6036bfb38
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-01-06 18:40:49 +0530

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,21 @@
+def twoSum(nums,target):
+        lst=[]
+        nums.sort()
+        for i in range(len(nums)) :
+            t=nums[i]
+            l = i + 1
+            r = len(nums)
+            while l < r :
+                if target - nums[l] == t :
+                    lst.append(i)
+                    lst.append(l)
+                    return lst
+                else :
+                    l=l+1
+                    
+                    
+
+
+l = [2,8,7,4]
+target = 9
+print(twoSum(l,target))
