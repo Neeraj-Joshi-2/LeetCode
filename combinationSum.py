24e0aba5efcdabd74e8a6bcdd3ef99f250b49b96
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-01-16 18:37:42 +0530

    Create combinationSum.py

---
+++ combinationSum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        res = []
+        candidates.sort()
+        
+        def dfs(target, index, path):
+            if target < 0:
+                return  # backtracking
+            if target == 0:
+                res.append(path)
+                return 
+            for i in range(index, len(candidates)):
+                dfs(target-candidates[i], i, path+[candidates[i]])
+        
+        dfs(target, 0, [])
+        return res
