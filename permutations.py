f370d3e489190202e6d51520da5b095268736cab
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-01-31 20:15:50 +0530

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,3 +1,19 @@
+def permuteUnique(nums):
+    res = []
+    nums.sort()
+    dfs(nums, [], res)
+    return res
+    
+def dfs(nums, path, res):
+    if not nums:
+        res.append(path)
+    for i in range(len(nums)):
+
+        dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+        
+print(permuteUnique([1,1,2]))
+
+
 def permute(nums):
     def backtrack(start, end):
         if start == end:
