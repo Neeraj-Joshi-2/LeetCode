312847d37cfd509098c0ee8057ac8018ce54c00d
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-02-02 20:53:17 +0530

    Create mergeSortedArray.py

---
+++ mergeSortedArray.py
@@ -0,0 +1,31 @@
+def merge(nums1, m, nums2, n):
+    while m > 0 and n > 0:
+        if nums1[m-1] >= nums2[n-1]:
+            nums1[m+n-1] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
+    if n > 0:
+        nums1[:n] = nums2[:n]
+        
+    print(nums1)
+        
+      
+    
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+merge(nums1 , m , nums2 , n)
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        n = len(nums1)-len(nums2)
+        j = 0
+        for i in range(n,len(nums1)) :
+            nums1[i] = nums2[j]
+            j+=1
+        nums1.sort()
+        return nums1
