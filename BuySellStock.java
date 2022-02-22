57b70dc99aa51a49cd688a23e2448b6ba1a2a8c8
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-02-22 23:37:34 +0530

    Update BuySellStock.java

--- BuySellStock.java
+++ BuySellStock.java
@@ -6,7 +6,7 @@ public class BuySellStock {
             currentProfit = prices[right] - prices[left];    
             if(prices[left] < prices[right]){ 
                 profit = Math.max(currentProfit,profit) ; 
-            }  
+            }   
             else{ 
                 left = right ;
             }
