a39c514dd19381c7d52abc4c15438e664bc5ec6b
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-03-08 19:52:42 +0530

    Create DetectCycleLinkedList.java

---
+++ DetectCycleLinkedList.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class DetectCycleLinkedList {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<ListNode>() ;
+        while(head!=null){
+            if(set.contains(head)){
+                return true ;
+            }
+            set.add(head) ;
+            head = head.next ;
+        }
+        return false ;        
+    }
+}
