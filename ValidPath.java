9a685a93aa78611a00015884fc06b764ba76191b
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-03-14 23:12:58 +0530

    Create ValidPath.java

---
+++ ValidPath.java
@@ -0,0 +1,16 @@
+class ValidPath {
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/");            
+        Stack<String> st = new Stack<>();
+        for(String s: paths) {                             
+            if(s.equals("")) continue;
+            if(s.equals(".")) continue;
+            if(s.equals("..")) 
+                if(!st.isEmpty()) st.pop();  
+                else continue;
+            else st.add(s);
+        }
+        System.out.println(st) ;
+        return "/" + String.join("/", st);        
+    }
+}
