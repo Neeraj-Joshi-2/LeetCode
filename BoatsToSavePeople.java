7613cd5abbfab0010334f65694342f9828ee277d
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-03-24 20:04:42 +0530

    Create BoatsToSavePeople.java

---
+++ BoatsToSavePeople.java
@@ -0,0 +1,17 @@
+class BoatsToSavePeople{
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people) ;
+        int start=0, end=people.length-1, count=0 ;
+        while(start<=end){
+            if(people[start]+people[end]<=limit){
+                start++;
+                end--;
+            }
+            else{
+                end-- ;
+            }
+            count++ ;
+        }
+        return count ;
+    }
+}
