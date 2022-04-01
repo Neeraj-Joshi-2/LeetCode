d6583b2833f352c18dde83e7009155ae632c1f74
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2022-04-01 19:20:25 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+class ReverseString {
+    public void reverseString(char[] s) {
+        char temp ;
+        int start = 0 , end = s.length-1 ;
+        while(start<end){
+            temp = s[start] ;
+            s[start] = s[end] ;
+            s[end] = temp ;
+            start++ ;
+            end-- ;
+        }
+    }
+}
